ae21bb51e37e20a46a8f44bb69b3378be444db75
luna <101215666+lunaera@users.noreply.github.com>
2026-09-04 17:45:08 -0600

    Add factorial2 function to handle negativo

--- factorial.js
+++ factorial.js
@@ -14,3 +14,12 @@ console.log(factorial(5)); // Resultado: 120
 console.log(factorial(23));
 
 
+function factorial2(n) {
+    if (n < 0) {
+        return "No existe factorial para números negativos";
+    }
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
